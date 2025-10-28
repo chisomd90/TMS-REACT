bf6410bb8da7270bb86d30d4cc980e04c16573dd
Stoney <chisomd90@gmail.com>
2025-10-28 23:40:42 +0100

    new fixes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Footer from '../components/Footer'
 import Hero from '../components/Hero'
-import Navbar from "../components/NavBar"
+import Navbar from "../components/Navbar"
 
 const Home = () => {
   return (
